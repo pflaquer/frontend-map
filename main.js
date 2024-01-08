a3abd0a3688fe66ee8a0087129ca884f5e2c69bf
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-01-08 15:48:07 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,130 @@
+var map = null;
+var buskers = [];
+window.onload = function() {
+	
+	var myOptions = {
+	
+		center: new google.maps.LatLng(40.6958, -73.9120),
+		zoom: 13,
+		scrollwheel: false,
+		disableDefaultUI: true,
+	};
+
+	map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+let buskers = [
+	{lat: 40.6958, lng: -73.9171},
+	{lat: 40.6960, lng: -73.9120}
+
+];
+  
+  console.log(buskers)
+
+buskers.forEach((x)=>{
+	var sucursal1 = new google.maps.Marker({
+		position: x,
+		map: map,
+		animation: google.maps.Animation.DROP,
+    title:'Artist: Lil Wayne, Genre: Hiphop, Soundcloud: www.soundcloud.com/lilwayne'
+		//icon: img
+	});
+}
+);
+
+};
+
+let state = false;
+
+
+function broadcast(x){
+if(state==true){
+  x = new google.maps.Marker({
+		position: {lat: 40.700, lng: -73.9171},
+    //{lat: parseInt(window.prompt('Insert Lat')), lng: parseInt(window.prompt('Insert Long'))},
+		map: map,
+		animation: google.maps.Animation.DROP,
+    title:'Artist: Lil Wayne, Genre: Hiphop, Soundcloud: www.soundcloud.com/lilwayne'
+		//icon: img
+	});
+}
+  else{alert('Busking Ended!')}
+  
+}
+
+var point = new google.maps.LatLng(40.7010, -73.9120);
+    var data = "Hello World!";
+    var infowindow = new google.maps.InfoWindow({
+      content: data
+    });
+
+
+    var marker = new google.maps.Marker({
+        position: point,
+        title:"Hello World!"
+    });
+
+
+
+google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
+
+let counter = 0;
+
+    const socket = io({
+      auth: {
+        serverOffset: 0
+      },
+      ackTimeout: 10000,
+      retries: 3,
+    });
+
+    const btn = document.getElementById('broadcast');
+
+
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      if(state==true){
+    
+        const clientOffset = `${socket.id}-${counter++}`;
+        socket.emit('chat message', window.prompt('Input Coordinates'), clientOffset);
+      }
+      else{}
+      
+    });
+
+    socket.on('chat message', (msg, serverOffset) => {
+      
+      
+       new google.maps.Marker({
+		position: {lat: 40.700, lng: -73.9171},
+    //{lat: parseInt(window.prompt('Insert Lat')), lng: parseInt(window.prompt('Insert Long'))},
+		map: map,
+		animation: google.maps.Animation.DROP,
+		//icon: img
+	});
+
+      
+      
+    });
+
+function handleClick(e){
+  alert(JSON.stringify(e))
+}
+
+
+
+let btns = document.getElementById("pulse")
+
+btns.onclick = (e)=>{
+  btns.classList.toggle('button-ani');
+  console.log(e);
+  state = !state;
+  alert('Busking Status Set to '+state);
+	buskers.push(this);	
+	let msg = 'Currently Busking='+buskers.length;
+	
+
+	
+}
